4ca04b81d61e41f846eb843442f75c060adbadcf
Wole Solana <osolana@thoughtworks.com>
2013-06-12 10:44:07 +0200

    Changes made to Basket Kata

--- Java/Katas/KataPotter/src/Basket.java
+++ Java/Katas/KataPotter/src/Basket.java
@@ -1,27 +1,33 @@
 import java.util.*;
 
-public class Basket {
+
+public class Basket extends ArrayList<Book>{
 
     private static final double BOOK_PRICE = 8.0;
-    private final List<Book> bookList = new ArrayList<Book>();
     private static final Map<Integer, Double> DISCOUNTS =
             Collections.unmodifiableMap(new HashMap<Integer, Double>() {{
-                put(1, 8d);
+                put(1, 8.0);
                 put(2, 15.2);
                 put(3, 21.6);
                 put(4, 25.6);
-                put(5, 30d);
+                put(5, 30.0);
             }});
+
+
+
+    public Basket(Book... books) {
+        Collections.addAll(this, books);
+    }
 
     public double calculatePrice() {
         double price = 0.0d;
 
-        while (!bookList.isEmpty()) {
+        while (!this.isEmpty()) {
 
             List<Book> uniqueBooks = new ArrayList<Book>();
 
             for (Book book : Book.values()) {
-                boolean remove = bookList.remove(book);
+                boolean remove = this.remove(book);
                 if (remove) {
                     uniqueBooks.add(book);
                 }
@@ -34,10 +40,5 @@ public class Basket {
         }
 
         return price;
-
-    }
-
-    public void addBook(Book book) {
-        bookList.add(book);
     }
 }
