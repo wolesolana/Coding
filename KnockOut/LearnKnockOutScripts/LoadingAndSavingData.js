f364bd4ac54819f12e78d970518b1be69b84bde6
Wole Solana <osolana@thoughtworks.com>
2013-06-23 15:20:27 +0200

    More knockout tutorials

---
+++ KnockOut/LearnKnockOutScripts/LoadingAndSavingData.js
@@ -0,0 +1,39 @@
+function Task(data) {
+    this.title = ko.observable(data.title);
+    this.isDone = ko.observable(data.isDone);
+}
+
+function TaskListViewModel() {
+    // Data
+    var self = this;
+    self.tasks = ko.observableArray([]);
+    self.newTaskText = ko.observable();
+    self.incompleteTasks = ko.computed(function() {
+        return ko.utils.arrayFilter(self.tasks(), function(task) { return !task.isDone() && !task._destroy });
+    });
+
+    // Operations
+    self.addTask = function() {
+        self.tasks.push(new Task({ title: this.newTaskText() }));
+        self.newTaskText("");
+    };
+    self.removeTask = function(task) { self.tasks.destroy(task) };
+    
+    /* Alternative
+    self.removeTask = function(task) { self.tasks.remove(task) };
+    */
+    
+    $.getJSON("/tasks", function(allData) { 
+        var mappedTasks = $.map(allData, function(item) { return new Task(item) });
+    });
+    
+    self.save = function() { 
+        $.ajax("/tasks", { 
+            data: ko.toJSON({ tasks: self.tasks }),
+            type: "post", contentType: "application/json",
+            success: function(result) { alert(result)}         
+        });
+    };
+}
+
+ko.applyBindings(new TaskListViewModel());
