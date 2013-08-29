d67225770a6a06bbc6d024c05b18e87f07905703
Wole Solana <osolana@thoughtworks.com>
2013-08-29 09:28:25 +0200

    adding rails tut to my repo

---
+++ RubyOnRails/first_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '62cd6ecb9c47d0bd25937bec6a37fbee7191fd2ccf7aed86974fb7d073717c4bc383ea2ed3cb907c6a16e7d745f9f8847e38c2b0bbaa7808370c4b3ff54d6f6d'
